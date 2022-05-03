393af2a792f57a671098baba83b8f0ea17704ab9
Rakhattt <67402354+Rakhattt@users.noreply.github.com>
2022-05-03 16:57:34 +0600

    dd

---
+++ dist/precache-manifest.6f75deff47ce3230be11ec776eeb7fd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67327c07b723cf8b02ba",
+    "url": "/css/app.364a7abf.css"
+  },
+  {
+    "revision": "38d502d9712e5c545893263045d1b895",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67327c07b723cf8b02ba",
+    "url": "/js/app.7028fe4a.js"
+  },
+  {
+    "revision": "263dda4b25bc481153eb",
+    "url": "/js/chunk-vendors.760c38c5.js"
+  },
+  {
+    "revision": "903c1ce6c51adad8ce0b9cf5022044da",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h1 class="bg-red-500 pt-6 text-lg font-medium">Hello world 2</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import '@/assets/tailwind.css'
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
