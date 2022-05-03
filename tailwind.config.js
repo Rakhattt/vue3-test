6333cadece596b8756c46ed71c76a93fa6c0953a
Rakhattt <67402354+Rakhattt@users.noreply.github.com>
2022-05-03 16:56:30 +0600

    s

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  addons: ['@storybook/addon-postcss'],
+  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
